7cc1fe1e4335b973e152b180e373c3de1f2b2041
sorrycc <sorrycc@gmail.com>
2023-10-12 14:11:47 +0800

    refact: align with less-loader

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const path = require("path");
-const fs = require("fs");
+const enhancedResolve = require("enhanced-resolve");
+
+/* eslint-disable class-methods-use-this */
+const trailingSlash = /[/\\]$/;
+
+// This somewhat changed in Less 3.x. Now the file name comes without the
+// automatically added extension whereas the extension is passed in as `options.ext`.
+// So, if the file name matches this regexp, we simply ignore the proposed extension.
+const IS_SPECIAL_MODULE_IMPORT = /^~[^/]+$/;
+
+// `[drive_letter]:\` + `\\[server]\[share_name]\`
+const IS_NATIVE_WIN32_PATH = /^[a-z]:[/\\]|^\\\\/i;
+
+// Examples:
+// - ~package
+// - ~package/
+// - ~@org
+// - ~@org/
+// - ~@org/package
+// - ~@org/package/
+const IS_MODULE_IMPORT =
+  /^~([^/]+|[^/]+\/|@[^/]+[/][^/]+|@[^/]+\/?|@[^/]+[/][^/]+\/)$/;
+const MODULE_REQUEST_REGEX = /^[^?]*~/;
 
 function parseOptions(options) {
     return options.split('&').reduce((acc, cur) => {
@@ -9,25 +30,6 @@ function parseOptions(options) {
     }, {});
 }
 
-const checkExtList = [".less", ".css"];
-function normalizePath(filename) {
-    if (/\.(?:less|css)$/i.test(filename)) {
-        return fs.existsSync(filename) ? filename : undefined;
-    }
-    for (let i = 0, len = checkExtList.length; i < len; i++) {
-        const ext = checkExtList[i];
-        if (fs.existsSync(`${filename}${ext}`)) {
-            return `${filename}${ext}`;
-        }
-    }
-}
-
-const MODULE_REQUEST_REGEX = /^[^?]*~/;
-
-const defaultLogger = {
-    log: console.log,
-    error: console.error
-};
 class LessAliasesPlugin {
     constructor(options) {
         this.options = options;
@@ -39,83 +41,125 @@ class LessAliasesPlugin {
         };
     }
     install(less, pluginManager) {
-        const { prefix = '', aliases, logger = defaultLogger } = this.options;
-        function resolve(filename) {
-            const chunks = filename.split('/');
-            const aliaseKey = chunks[0];
-            const restPath = chunks.slice(1).join('/');
-            const resolvedAliase = aliases[aliaseKey];
-            let resolvedPath = normalizePath(path.join(resolvedAliase, restPath));
-            if (!resolvedPath) {
-                throw new Error(`Invalid aliase config for key: ${aliaseKey}`);
-            }
-            return resolvedPath;
-        }
-        class AliasePlugin extends less.FileManager {
-            supports(filename, currentDirectory) {
-                const aliaseNames = Object.keys(aliases);
-                const len = aliaseNames.length;
-                for (let i = 0; i < len; i++) {
-                    const key = `${prefix}${aliaseNames[i]}`;
-                    if (filename === key || filename.startsWith(`${key}/`)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-            supportsSync(filename, currentDirectory) {
-                return this.supports(filename, currentDirectory);
-            }
-            loadFile(filename, currentDirectory, options, enviroment, callback) {
-                let resolved;
-                try {
-                    resolved = resolve(filename);
-                }
-                catch (error) {
-                    logger.error(error);
-                }
-                if (!resolved) {
-                    const error = new Error(`[less-plugin-aliases]: '${filename}' not found.`);
-                    logger.error(error);
-                    throw error;
-                }
-                return super.loadFile(resolved, currentDirectory, options, enviroment, callback);
-            }
-            loadFileSync(filename, currentDirectory, options, enviroment, callback) {
-                let resolved;
-                try {
-                    resolved = resolve(filename);
-                }
-                catch (error) {
-                    logger.error(error);
-                }
-                if (!resolved) {
-                    const error = new Error(`[less-plugin-aliases]: '${filename}' not found.`);
-                    logger.error(error);
-                    throw error;
-                }
-                return super.loadFileSync(resolved, currentDirectory, options, enviroment, callback);
-            }
-        }
-        pluginManager.addFileManager(new AliasePlugin());
+        const { aliases } = this.options;
 
-        class StripTildePrefix extends less.FileManager {
-            supports(filename, currentDirectory) {
-                return true;
-            }
-            supportsSync(filename, currentDirectory) {
-                return true;
-            }
-            loadFile(filename, currentDirectory, options, enviroment, callback) {
-                let resolved = filename.startsWith('~') ? filename.replace(MODULE_REQUEST_REGEX, '') : filename;
-                return super.loadFile(resolved, currentDirectory, options, enviroment, callback);
-            }
-            loadFileSync(filename, currentDirectory, options, enviroment, callback) {
-                let resolved = filename.startsWith('~') ? filename.replace(MODULE_REQUEST_REGEX, '') : filename;
-                return super.loadFileSync(resolved, currentDirectory, options, enviroment, callback);
-            }
+        const resolver = enhancedResolve.create({
+            alias: aliases,
+            dependencyType: "less",
+            conditionNames: ["less", "style", "..."],
+            mainFields: ["less", "style", "main", "..."],
+            mainFiles: ["index", "..."],
+            extensions: [".less", ".css"],
+            preferRelative: true,
+        });
+
+        async function resolve(context, path) {
+            return new Promise((resolve, reject) => {
+                resolver(context, path, (err, result) =>
+                    err ? reject(err) : resolve(result),
+                );
+            });
         }
-        pluginManager.addFileManager(new StripTildePrefix());
+
+        class ResolvedFileManager extends less.FileManager {
+          supports(filename) {
+            if (filename[0] === "/" || IS_NATIVE_WIN32_PATH.test(filename)) {
+              return true;
+            }
+      
+            if (this.isPathAbsolute(filename)) {
+              return false;
+            }
+      
+            return true;
+          }
+      
+          // Sync resolving is used at least by the `data-uri` function.
+          // This file manager doesn't know how to do it, so let's delegate it
+          // to the default file manager of Less.
+          // We could probably use loaderContext.resolveSync, but it's deprecated,
+          // see https://webpack.js.org/api/loaders/#this-resolvesync
+        supportsSync() {
+            return false;
+        }
+
+        async resolveFilename(filename, currentDirectory) {
+            // Less is giving us trailing slashes, but the context should have no trailing slash
+            const context = currentDirectory.replace(trailingSlash, "");
+
+            let request = filename;
+
+            // A `~` makes the url an module
+            if (MODULE_REQUEST_REGEX.test(filename)) {
+                request = request.replace(MODULE_REQUEST_REGEX, "");
+            }
+
+            if (IS_MODULE_IMPORT.test(filename)) {
+                request = request[request.length - 1] === "/" ? request : `${request}/`;
+            }
+
+            return this.resolveRequests(context, [...new Set([request, filename])]);
+        }
+      
+          async resolveRequests(context, possibleRequests) {
+            if (possibleRequests.length === 0) {
+              return Promise.reject();
+            }
+      
+            let result;
+      
+            try {
+                result = await resolve(context, possibleRequests[0]);
+            } catch (error) {
+                const [, ...tailPossibleRequests] = possibleRequests;
+
+                if (tailPossibleRequests.length === 0) {
+                    throw error;
+                }
+
+                result = await this.resolveRequests(context, tailPossibleRequests);
+            }
+      
+            return result;
+          }
+      
+          async loadFile(filename, ...args) {
+            let result;
+      
+            try {
+              if (IS_SPECIAL_MODULE_IMPORT.test(filename)) {
+                const error = new Error();
+      
+                error.type = "Next";
+      
+                throw error;
+              }
+      
+              result = await super.loadFile(filename, ...args);
+            } catch (error) {
+              if (error.type !== "File" && error.type !== "Next") {
+                return Promise.reject(error);
+              }
+      
+              try {
+                result = await this.resolveFilename(filename, ...args);
+              } catch (webpackResolveError) {
+                error.message =
+                  `Less resolver error:\n${error.message}\n\n` +
+                  `Enhanced resolver error details:\n${webpackResolveError.details}\n\n` +
+                  `Enhanced resolver error missing:\n${webpackResolveError.missing}\n\n`;
+      
+                return Promise.reject(error);
+              }
+      
+              return super.loadFile(result, ...args);
+            }
+      
+            return result;
+          }
+        }
+      
+        pluginManager.addFileManager(new ResolvedFileManager());
     }
 }
 
